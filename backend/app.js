22694e12ecad5d82bb46babf563a9694cec063af
akshpnwr <akashpanwar9976@gmail.com>
2024-08-02 18:44:23 +0530

    serving static files

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,19 @@ dotenv.config()
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 
 app.use('/api/v1/products', productRouter);
 app.use('/products', express.static('products'));
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(PORT, async () => {
     await connectDB();
